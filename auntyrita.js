f4b4828e24960eb6f49c101fcece2a81260362d8
Addai325 <140252643+Addai325@users.noreply.github.com>
2023-12-18 21:33:18 -0800

    Update auntyrita.js

--- auntyrita.js
+++ auntyrita.js
@@ -43,3 +43,8 @@ var swiper = new Swiper('.swiper-container', {
     },
 });
 
+// Function to close the menu when a link is clicked
+function closeMenu() {
+    document.getElementById('menuToggle').checked = false;
+  }
+  
